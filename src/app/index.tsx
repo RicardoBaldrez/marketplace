84079922cc04ccedb4d2e77ab6416323ca2665ee
Ricardo Baldrez <rdbz@gft.com>
2026-01-21 20:25:44 -0300

    feat(ROUTER): Configure route with expo-router

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import { router } from "expo-router";
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+      <TouchableOpacity onPress={() => router.push('/login')}>
+        <Text>Login</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
