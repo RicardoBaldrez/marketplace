3379baf17c1c1665da362ba2923582b03cfe699b
Ricardo Baldrez <rdbz@gft.com>
2026-01-22 17:41:32 -0300

    feat(ROUTER): Configure private routes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,17 @@
 import { Redirect } from "expo-router";
 
 export default function App() {
+  const userData = {
+    token: '1234567890',
+    user: {
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+    },
+  }
+
+  if (userData) {
+    return <Redirect href="/(private)/home" />
+  }
+
   return <Redirect href="/login" />
 }
