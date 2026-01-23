4f7a67e19bc639488577965e35ff3a7a65d68491
Ricardo Baldrez <rdbz@gft.com>
2026-01-23 18:02:37 -0300

    feat(REGISTER): add register service and http/user interfaces

---
+++ src/shared/interfaces/http/register.ts
@@ -0,0 +1,15 @@
+import { IUser } from "../user";
+
+export interface RegisterHttpParams {
+  name: string;
+  email: string;
+  avatarUrl: string;
+  phone: string;
+  password: string;
+}
+
+export interface RegisterHttpResponse {
+  user: IUser;
+  token: string;
+  refreshToken: string;
+}
---
+++ src/shared/services/auths.service.ts
@@ -0,0 +1,7 @@
+import { RegisterHttpParams, RegisterHttpResponse } from "../interfaces/http/register";
+import { marketPlaceApiClient } from "../api/market-place";
+
+export const register = async (userData: RegisterHttpParams) => {
+  const { data } = await marketPlaceApiClient.post<RegisterHttpResponse>('/auth/register', userData);
+  return data;
+}
